4ec47609e3d49f3f02c6ae8e68d94d6bb02b1777
Rylexium <aleksei.azizov@mail.ru>
2022-02-24 00:56:22 +0400

    fadsfsdf

--- src/main/java/com/page/vkr/controllers/AuthorizationController.java
+++ src/main/java/com/page/vkr/controllers/AuthorizationController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("authorization")
 public class AuthorizationController {
@@ -20,7 +22,7 @@ public class AuthorizationController {
     private AbitRepository abitRepository;
 
     @GetMapping
-    public AuthorizationInfo authorization(@RequestParam(value = "login")      String login,
+    public Object authorization(@RequestParam(value = "login")      String login,
                                            @RequestParam(value = "password")   String password) {
         Users user = usersRepository.findUserByLogin(login);
         if(user != null && user.getPassword().equals(HashPass.getHashSha256(password, user.getSalt1(), user.getSalt2()))){
@@ -29,6 +31,6 @@ public class AuthorizationController {
                     .idEducation(abitRepository.findEducationById(user.getId_abit()))
                     .build();
         }
-        return null;
+        return new HashMap<String, String>() {{put("status", "failed");}};
     }
 }
