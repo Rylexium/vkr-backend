8107c030cf0ef47347f793ba049f6a43cbee79d5
Rylexium <aleksei.azizov@mail.ru>
2022-02-26 18:13:06 +0400

    cache

--- src/main/java/com/page/vkr/repo/ExamsRepository.java
+++ src/main/java/com/page/vkr/repo/ExamsRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExamsRepository extends JpaRepository<Exams, Integer> {
     @Query(value = "select * from exams where id=:id", nativeQuery = true)
     Optional<Exams> findById(@Param("id") Integer id);
+
+    @Query(value = "select * from exams order by id", nativeQuery = true)
+    List<Exams> findAll();
 }
