a43f6d25b4d83db1fcc44429fb09765d79b325e0
Rylexium <aleksei.azizov@mail.ru>
2022-03-22 23:46:46 +0400

    order by id in education and type_of_study

--- src/main/java/com/page/vkr/repo/EducationRepository.java
+++ src/main/java/com/page/vkr/repo/EducationRepository.java
@@ -2,7 +2,11 @@ package com.page.vkr.repo;
 
 import com.page.vkr.models.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EducationRepository extends JpaRepository<Education, Integer> {
-
+    @Query(value = "select * from education order by id", nativeQuery = true)
+    List<Education> findAll();
 }
--- src/main/java/com/page/vkr/repo/TypeOfStudyRepository.java
+++ src/main/java/com/page/vkr/repo/TypeOfStudyRepository.java
@@ -2,6 +2,11 @@ package com.page.vkr.repo;
 
 import com.page.vkr.models.TypeOfStudy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TypeOfStudyRepository extends JpaRepository<TypeOfStudy, Integer> {
+    @Query(value = "select * from type_of_study order by id", nativeQuery = true)
+    List<TypeOfStudy> findAll();
 }
